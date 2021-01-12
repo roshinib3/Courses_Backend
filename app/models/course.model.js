25d4215249b46f8d908d006478e99916a7a2b9a7
Roshini Balasubramanian <roshini.bala123@gmail.com>
2021-01-12 14:00:15 -0500

    Update course.model.js

--- app/models/course.model.js
+++ app/models/course.model.js
@@ -1,3 +1,20 @@
 module.exports = (mongoose) => {
   // TODO: Compile a mongoose course model
+    const Course = mongoose.model(
+    "course",
+    mongoose.Schema(
+      {
+        title: String,
+        description: String,
+        author: String,
+        viewCount: Number,
+        imageUrl: String,
+        subject: String,
+        duration: String,
+      },
+      { timestamps: true }
+    )
+  );
+
+  return Course;
 };
